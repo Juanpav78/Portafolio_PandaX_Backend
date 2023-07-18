f3094803aa7f61b511ba7b7b959271b19151db25
Juanpav78 <juanpav.78d@gmail.com>
2023-07-18 16:54:09 -0500

    __Prueba de Error de Cors

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ const whiteList = [process.env.FRONTEND_URL
 const corsOptions = {
 
     origin: function(origin, callback){
+        console.log(origin)
+        console.log(process.env.FRONTEND_URL)
         if(whiteList.includes(origin)){
             // puede consultar la api
             callback(null, true)
